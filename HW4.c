0441a54a72196ee1aff63cba9bcc47ab7faf3943
haoyuan1215 <147589517+haoyuan1215@users.noreply.github.com>
2023-10-31 23:11:24 +0800

    Add files via upload

--- HW4.c
+++ HW4.c
@@ -1,17 +1,69 @@
 #include <stdio.h>
-
-int main() {
-    int n,m=1,dx=-1,dy=0,i,j;
+int main()   {
     int chess[8][8]={{0,0,0,0,0,0,0,0},
                      {0,0,0,0,0,0,0,0},
                      {0,0,0,0,0,0,0,0},
-                     {0,0,0,2,1,0,0,0},
+                     {0,2,2,2,1,0,0,0},
                      {0,0,0,1,2,0,0,0},
-                     {0,0,0,0,0,0,0,0},
-                     {0,0,0,0,0,0,0,0},
+                     {0,0,0,0,2,0,0,0},
+                     {0,0,0,0,2,0,0,0},
                      {0,0,0,0,0,0,0,0}};
+    for (int i=0;i<8;i++) {
+        for(int j=0;j<8;j++) {
+            printf("%d",chess[i][j]);
+        }
+        printf("\n");
+    }
+    int i,j,n=1;
     scanf("%d%d",&i,&j);
-
-    printf("%d\n%d",i+m*dx,j+m*dy);
+    if(chess[i][j]==0) {
+        printf("ªÅ");
+    }
+    if (chess[i][j]==1) {
+        for(int dx=-1;dx<=1;dx++) {
+            for(int dy=-1;dy<=1;dy++) {
+                if(chess[i+dx][j+dy]==2) {
+                    while(chess[i+n*dx][j+n*dy]!=0) {
+                        if(i+n*dx<0||i+n*dx>=8||j+n*dy<0||j+n*dy>=8||chess[i+n*dx][j+n*dy]==1) {
+                            n=0;
+                            break;
+                        }
+                        else {
+                            n++;
+                        }
+                    }
+                    if(n>0) {
+                        printf("%d(%d,%d)\n",n,dx,dy);
+                    }
+                    else{
+                        printf("0");
+                    }
+                }
+            }
+        }
+    }
+    if (chess[i][j]==2) {
+        for(int dx=-1;dx<=1;dx++) {
+            for(int dy=-1;dy<=1;dy++) {
+                if(chess[i+dx][j+dy]==1) {
+                    while(chess[i+n*dx][j+n*dy]!=0) {
+                        if(i+n*dx<0||i+n*dx>=8||j+n*dy<0||j+n*dy>=8||chess[i+n*dx][j+n*dy]==2) {
+                            n=0;
+                            break;
+                        }
+                        else {
+                            n++;
+                        }
+                    }
+                    if(n>0) {
+                        printf("%d(%d,%d)\n",n,dx,dy);
+                    }
+                    else {
+                        printf("0");
+                    }
+                }
+            }
+        }
+    }
     return 0;
 }
